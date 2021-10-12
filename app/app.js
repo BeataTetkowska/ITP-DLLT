dc4bc95e639ddf5aaa7f7a3053f72fb29203dcf5
JoaquimXG <49433786+JoaquimXG@users.noreply.github.com>
2021-10-12 15:51:29 +0100

    DLLT-60 Setting up testing with jest and supertest for api tests
    I have set up the outline for the first /event route and an example test
    to show how the testing and api code can be structured

--- app/app.js
+++ app/app.js
@@ -2,9 +2,12 @@ const express = require("express");
 const app = express();
 
 const logger = require("./middleware/logger");
+const eventRouter = require("./routes/event");
 
 //Logging with winston used as middleware to log all requests made to backend
 app.use(logger);
+
+app.use("/event", eventRouter);
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
---
+++ app/routes/event.js
@@ -0,0 +1,16 @@
+const express = require('express')
+var router = express.Router();
+
+//An example of what data may be included within a single event
+currentEventExample = {
+  id: 1,
+  ts: new Date().toISOString(),
+  location: "Cruyff Court"
+}
+
+//Example route for sending only the JSON data for a single event
+router.get("/", (req, res) => {
+  res.json(currentEventExample)
+})
+
+module.exports = router;
