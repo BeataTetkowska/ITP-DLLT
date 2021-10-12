e24dc6b15b38481961c3d1e2a948ecc08e05f70a
JoaquimXG <49433786+JoaquimXG@users.noreply.github.com>
2021-10-12 15:51:29 +0100

    DLLT-60 Setting up testing with jest and supertest for api tests
    I have set up the outline for the first /event route and an example test
    to show how the testing and api code can be structured

---
+++ test/routes/event.test.js
@@ -0,0 +1,18 @@
+const request = require("supertest");
+const app = require("../../app/app");
+
+describe("/event", () => {
+  it("GET /event -> current event", async () => {
+    return request(app)
+      .get("/event")
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .then((res) => {
+        expect(res.body).toEqual(expect.objectContaining({
+          id: expect.any(Number),
+          ts: expect.any(String),
+          location: expect.any(String)
+        }));
+      });
+  });
+});
