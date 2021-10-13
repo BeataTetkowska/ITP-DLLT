760c0ee1b788acb7425618c6897030e19aec0cb5
JoaquimXG <49433786+JoaquimXG@users.noreply.github.com>
2021-10-13 19:39:42 +0100

    DLLT-64 Created placeholder sign up form for testing ajax function

--- app/routes/signup.js
+++ app/routes/signup.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 var router = express.Router();
 const log = require("../utils/winstonLogger");
@@ -7,7 +8,7 @@ const users = require("../db/users");
 
 //GET /signup -> returns html for signup page
 router.get("/", (req, res) => {
-  res.send("Signup Page");
+  res.sendFile(path.join(__dirname, "../views/signup.html"));
 });
 
 //POST /signup -> creates user
