7dbfd4a1392381007a353fb18eee2e888173c5f3
JoaquimXG <49433786+JoaquimXG@users.noreply.github.com>
2021-10-13 15:32:21 +0100

    DLLT-51 Completed endpoint for /api/event
    Sends json data for the next event

--- app/routes/event.js
+++ app/routes/event.js
@@ -5,18 +5,60 @@ var apiRouter = express.Router();
 var events = require("../db/event");
 
 //GET /event -> returns html for event page
-viewRouter.get("/", (req, res) => {
+viewRouter.get("/", (_, res) => {
   //TODO send html
   res.send("Events page");
 });
 
 //GET /api/event -> returns json data for next event
-apiRouter.get("/", (req, res) => {
-  //TODO find next event
-  res.json(events[0]);
+apiRouter.get("/", (_, res) => {
+  //Get current date and time
+  var now = new Date();
+
+  nextEvent = getNextEvent(now);
+
+  res.json(nextEvent);
 });
 
 module.exports = {
   view: viewRouter,
   api: apiRouter,
 };
+
+module.exports = {
+  view: viewRouter,
+  api: apiRouter,
+};
+
+//Searches through the list of events to find any events on today
+//Returns false if no events can be found that are on today
+function getNextEventToday(time) {
+  var upcomingEventsToday = events
+    .filter((event) => time.getUTCDay() == event.day)
+    .filter((event) => time.getHours() < event.start.hours);
+
+  var nextEvent;
+  if (upcomingEventsToday.length > 0) {
+    nextEvent = upcomingEventsToday.reduce((prev, current) =>
+      prev.hours < current.hours ? prev : current
+    );
+    return nextEvent;
+  } else {
+    return false;
+  }
+}
+
+//Finds the next event
+//Will try to find the next event today
+//if none is found, will find the first event for the next day
+function getNextEvent(time) {
+  var nextEventToday = getNextEventToday(time);
+  if (nextEventToday) {
+    return nextEventToday;
+  } else {
+    time.setDate(time.getDate() + 1);
+    time.setHours(0);
+    nextEvent = getNextEvent(time);
+  }
+  return nextEvent;
+}
