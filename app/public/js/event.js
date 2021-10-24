e319452cbb36c8b208fd0c5e2073b6db1dfda38d
JoaquimXG <49433786+JoaquimXG@users.noreply.github.com>
2021-10-24 10:56:51 +0100

    DLLT-74 Creating ajax POST call to allow user to register for event.
    Fields which are being sent may require tweaking in future commits

--- app/public/js/event.js
+++ app/public/js/event.js
@@ -1,3 +1,4 @@
+var scheduleId;
 $(function () {
   var url = "/api/event";
   var days = [
@@ -10,7 +11,10 @@ $(function () {
     "Saturday",
   ];
 
+  //Get JSON data about the currently on event 
+  //Parse data into html
   $.getJSON(url, function (data) {
+    scheduleId = data._id;
     $("#location").text(data.location);
     $("#time").text(
       `${data.start.hours}:${
@@ -19,4 +23,40 @@ $(function () {
     );
     $("#day").text(days[data.day]);
   });
+
+  $("#register").on("click", registerEvent);
 });
+
+//Sends data about the current event to the server to attempt to register
+//the currently logged in user for the event
+function registerEvent() {
+  var now = new Date();
+
+  var url = "/api/event/register";
+  $.post({
+    type: "POST",
+    url: url,
+    data: {
+      scheduleId,
+      date: now.getDate(),
+      month: now.getMonth(),
+      year: now.getYear(),
+    },
+    dataType: "json",
+  })
+    .done((response) => {
+      //Notify user if registration was succesful
+      if (response.result.success === true) {
+        $("#register").css({color: "green", "border-color":"green"})
+          .text("Registered")
+      }
+      //If user is not signed in, notify user
+      else {
+        //TODO Navigate user to the sign in page, or request more information
+        alert("User is not signed in");
+      }
+    })
+    .fail(() => {
+      //TODO handle server failure
+    });
+}
