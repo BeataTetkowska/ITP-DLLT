0620d318914fe5abf842257c0cc696bd17362464
JoaquimXG <49433786+JoaquimXG@users.noreply.github.com>
2021-11-07 18:55:57 +0000

    Updating jest to show failure messages in http report

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,12 @@
 const config = {
+  testTimeout: 500,
   reporters: [
     "default",
     [
       "./node_modules/jest-html-reporter",
       {
         pageTitle: "Test Report",
+        includeFailureMsg: true,
       },
     ],
   ],
