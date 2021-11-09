7b608e773138bc69c3fb36bab9a0b03998dfc38d
JoaquimXG <49433786+JoaquimXG@users.noreply.github.com>
2021-11-09 22:02:51 +0000

    Tests for add user

---
+++ test/routes/user/add.test.js
@@ -0,0 +1,30 @@
+const request = require("supertest");
+const app = require("../../../app/app");
+const loginUser = require("../../utils/loginUser");
+const server = request.agent(app);
+
+describe("GET /user/add no auth", () => {
+  var url = "/user/add";
+  it("-> HTTP 200", () => request(app).get(url).expect(401));
+  it("-> Content Type HTML", () =>
+    request(app).get(url).expect("Content-type", /text/));
+});
+
+function loginAndGetAddUser(email, code) {
+  var url = "/user/add";
+  var password = email;
+
+  beforeAll(() => loginUser(server, email, password));
+
+  it("-> HTTP 403", () => server.get(url).expect(code));
+  it("-> Content Type HTML", () =>
+    server.get(url).expect("Content-type", /text/));
+
+  afterAll(() => server.get("/user/logout"));
+}
+
+describe("GET /user/add standard user", () =>
+  loginAndGetAddUser("email@taken.com", 403));
+
+describe("GET /user/add as admin", () =>
+  loginAndGetAddUser("admin@admin.admin", 200));
